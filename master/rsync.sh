ebee11fa7d14268a467b4294fa9667709c322ab0
Smart Lee <ismtlee@gmail.com>
2015-10-27 18:59:27 +0800

    .

--- master/rsync.sh
+++ master/rsync.sh
@@ -3,27 +3,27 @@
 export RSYNC_PASSWORD=passwd 
 export PATH=$PATH:/bin:/usr/bin:/usr/local/bin 
 
-// 设置远程服务器与需要备份的目录
+# 设置远程服务器与需要备份的目录
 SERVER=rsync@Server 
 DIR=(andplus cellar script deploy nginx cron etc profile)
 
-// 设置客户端上的用于备份的目录
+# 设置客户端上的用于备份的目录
 BDIR=/Backup 
 BASE=Current 
 INCREMENTDIR=`date +%Y-%m-%d` 
 
-// 在客户端灵活使用 exclude 功能；设置日志
+# 在客户端灵活使用 exclude 功能；设置日志
 EXCLUDES=/root/rsync/excludes 
 LOG=/tmp/rsync.log 
 
-// 参数设置，调用 backup-dir 将服务器端的增量文件存储到 INCREMENTDIR 目录下
+# 参数设置，调用 backup-dir 将服务器端的增量文件存储到 INCREMENTDIR 目录下
 OPTS="-avz--force --delete --delete-excluded  --exclude-from=$EXCLUDES -b  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
 #OPTS="-avz--force --delete --delete-excluded  --exclude-from=$EXCLUDES -b  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
 
-// 确保客户端上的用于备份的目录存在
+# 确保客户端上的用于备份的目录存在
 install -d $BDIR/$BASE 
 
-// 实现数据备份与同步，将日志邮寄给 root 用户
+# 实现数据备份与同步，将日志邮寄给 root 用户
 run_rsync() 
 { 
  echo "==========Begin rsync: `date`===========" >>$LOG 2>&1 
