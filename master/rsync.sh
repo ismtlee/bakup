440e940c7b83583dcfd35d484f819879d237af79
Smart Lee <ismtlee@gmail.com>
2015-10-27 18:55:20 +0800

    .

--- master/rsync.sh
+++ master/rsync.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 
 // 设置远程服务器与需要备份的目录
 SERVER=rsync@Server 
-DIR=CriticalData 
+DIR=(andplus cellar script deploy nginx cron etc profile)
 
 // 设置客户端上的用于备份的目录
 BDIR=/Backup 
@@ -17,7 +17,7 @@ EXCLUDES=/root/rsync/excludes
 LOG=/tmp/rsync.log 
 
 // 参数设置，调用 backup-dir 将服务器端的增量文件存储到 INCREMENTDIR 目录下
-OPTS="-avz --force --delete --delete-excluded  --exclude-from=$EXCLUDES -b 
+OPTS="-avz--force --delete --delete-excluded  --exclude-from=$EXCLUDES -b 
  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
 
 // 确保客户端上的用于备份的目录存在
@@ -28,10 +28,15 @@ run_rsync()
 { 
  echo "==========Begin rsync: `date`===========" >>$LOG 2>&1 
  du -s $BDIR/* >>$LOG 2>&1 
- rsync $OPTS $SERVER::$DIR $BDIR/$BASE  >>$LOG 2>&1 
+ 
+ for ary in ${DIR[@]} 
+ do echo $ary
+ done
+
+ #rsync $OPTS $SERVER::$DIR $BDIR/$BASE  >>$LOG 2>&1 
  echo "==========End rsync: `date`===========" >>$LOG 2>&1 
- mail root -s "Backup Report" < $LOG 
- rm $LOG 
+ #mail root -s "Backup Report" < $LOG 
+ #rm $LOG 
 } 
 
  // 条件测试，运行程序
@@ -41,6 +46,6 @@ if [ -f $EXCLUDES ]; then
        else 
                echo "cant find $BDIR"; exit 
        fi 
-else 
-       echo "cant find $EXCLUDES"; exit 
+       else 
+               echo "cant find $EXCLUDES"; exit 
 fi
