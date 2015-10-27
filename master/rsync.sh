3e84822c95807cc5395fc991032d295530b56fc1
Smart Lee <ismtlee@gmail.com>
2015-10-27 19:15:36 +0800

    .

--- master/rsync.sh
+++ master/rsync.sh
@@ -18,7 +18,7 @@ INCREMENTDIR=`date +%Y-%m-%d`
 LOG=/tmp/rsync.log 
 
 # 参数设置，调用 backup-dir 将服务器端的增量文件存储到 INCREMENTDIR 目录下
-OPTS="-avz--force --delete --exclude='.svn/***'   -b  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
+OPTS="-avz --force --delete --exclude='.svn/***'   -b  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
 #OPTS="-avz--force --delete --delete-excluded  --exclude-from=$EXCLUDES -b  --backup-dir=$BDIR/$INCREMENTDIR --ignore-errors"
 
 # 确保客户端上的用于备份的目录存在
