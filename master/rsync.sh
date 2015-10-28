fec7ff0090c647e8c906773fc1ac97fb0a5cdb4c
Smart Lee <ismtlee@gmail.com>
2015-10-28 17:42:31 +0800

    .

--- master/rsync.sh
+++ master/rsync.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 
 # 设置远程服务器与需要备份的目录
 SERVER=www@master.server
-DIR=(cellar script deploy nginx cron etc profile)
+DIR=(cellar scripts deploy nginx cron etc profile)
 #DIR=(cellar scripts)
 
 # 设置客户端上的用于备份的目录
