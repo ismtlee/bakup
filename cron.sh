ca8218439d9ca4a7f242894b2d832acc9994ef2c
Smart Lee <ismtlee@gmail.com>
2015-10-29 17:49:39 +0800

    .

--- cron.sh
+++ cron.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-/bin/sh /data/bakup/master/data/bakup/svr_master/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_slave/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_platdb/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_redis/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_res1/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_res2/rsync.sh
-/bin/sh /data/bakup/master/data/bakup/svr_res3/rsync.sh
+/bin/sh /data/bakup/svr_master/rsync.sh
+/bin/sh /data/bakup/svr_slave/rsync.sh
+/bin/sh /data/bakup/svr_platdb/rsync.sh
+/bin/sh /data/bakup/svr_redis/rsync.sh
+/bin/sh /data/bakup/svr_res1/rsync.sh
+/bin/sh /data/bakup/svr_res2/rsync.sh
+/bin/sh /data/bakup/svr_res3/rsync.sh
