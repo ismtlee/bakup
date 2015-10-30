28287f892920c9deffba3008fb475cd7558274ab
Smart Lee <ismtlee@gmail.com>
2015-10-30 16:15:55 +0800

    .

--- linkerboom/rsync.sh
+++ linkerboom/rsync.sh
@@ -1,2 +1,5 @@
 #rsync -avz -e ssh  --force --delete  --ignore-errors  root@client.bak:/Backup/ /Backup/
-rsync -Pav   --force --delete  --ignore-errors  client.bak:/Backup/  /Backup/
+LOG=/logs/rsync_bak_linkerboom.log
+echo "==========Begin rsync: `date`===========" >>$LOG 2>&1 
+rsync -Pav   --force --delete  --ignore-errors  client.bak:/Backup/  /Backup/ >> $LOG 2>&1
+echo "==========End rsync: `date`===========" >>$LOG 2>&1 
