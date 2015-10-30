42108c18d89bef1b33c44ca79f32128a9c6710ec
Smart Lee <ismtlee@gmail.com>
2015-10-30 16:09:21 +0800

    .

--- linkerboom/rsync.sh
+++ linkerboom/rsync.sh
@@ -1,9 +1,2 @@
-#!/bin/sh 
-#export RSYNC_PASSWORD=passwd 
-export PATH=$PATH:/bin:/usr/bin:/usr/local/bin 
-
-# 设置远程服务器与需要备份的目录
-SERVER=root@client.bak
-DIR=(linkerboom) 
-
-rsync -avz -e ssh  --force --delete  --ignore-errors  root@client.bak:/Backup /Backup
+#rsync -avz -e ssh  --force --delete  --ignore-errors  root@client.bak:/Backup/ /Backup/
+rsync -Pav   --force --delete  --ignore-errors  client.bak:/Backup/  /Backup/
